54763e7d517361f353dad8c26487dc98db4c77b5
Oliver Njiru <olivernjiru@gmail.com>
2022-05-28 14:23:38 +0300

    ratingpage

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-let professor = "Rao Jonalagadha Venkataswara";
-const rating = [1, 2, 3, 4, 5];
-var rating1 = window.prompt("Enter a rating for " + professor + "from 1 to 5: ");
-alert(professor + " rating is " + rating1);
+//let professor = "Rao Jonalagadha Venkataswara";
+//var rating = window.prompt("Enter a rating for " + professor + "from 1 to 5: ");
+//alert(professor + " rating is " + rating);
