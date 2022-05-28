cf756f92039a734677898fb7e0095c2af05fa5a2
Oliver Njiru <olivernjiru@gmail.com>
2022-05-28 13:52:05 +0300

    create javascript rating system branch

---
+++ main.js
@@ -0,0 +1,4 @@
+let professor = "Rao Jonalagadha Venkataswara";
+const rating = [1, 2, 3, 4, 5];
+var rating1 = window.prompt("Enter a rating for " + professor + "from 1 to 5: ");
+alert(professor + " rating is " + rating1);
